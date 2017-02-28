0e3b088dd2a0157604fdf03ad7d3064af22cea31
253510 <253510@PC276536.cts.com>
2017-02-28 21:51:47 +0530

    User1-Commit

--- src/test/java/api/PostRequest.java
+++ src/test/java/api/PostRequest.java
@@ -1,18 +1,36 @@
 package api;
 
-import cucumber.api.java.en.Given;
-import net.thucydides.core.annotations.Step;
-import stepdefinitions.Apidefinitions;
-import stepdefinitions.ServiceURI;
-import net.serenitybdd.*;
-import net.serenitybdd.rest.SerenityRest;
-public class PostRequest extends SerenityRest{
 
-@Step
+import net.thucydides.core.annotations.Step;
+import stepdefinitions.ServiceURI;
+import static net.serenitybdd.rest.SerenityRest.given;
+import static net.serenitybdd.rest.SerenityRest.then;
+import static net.serenitybdd.rest.SerenityRest.when;
+import static net.serenitybdd.rest.SerenityRest.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+
+
+import static net.serenitybdd.rest.SerenityRest.rest;
+
+
+
+
+
+public class PostRequest {
+	//public static ResponseSpecification response=null;
+//	public static RequestSpecification po = null;
+	String uriVal = null;
+	@Step
 public void postGETRequest(String uri){
-	String uriVal = getURI(uri);
+	uriVal = getURI(uri);
 	System.out.println(uriVal);
-	post(uriVal);
+	//post(uriVal);
+	//resp();
 }
 
 public String getURI(String uri){
@@ -25,12 +43,40 @@ public String getURI(String uri){
 		
 }
 
-public static void post(String uri){
-	 Apidefinitions.responsebody = 
-	 given().headers(LoadRequest.jsonAsMap)
-	 .proxy("proxy.cognizant.com", 6050)
-	 .get(uri)
-	 .andReturn();			 
+@Step
+public void giv(){
+	rest().given()
+	.proxy("proxy.cognizant.com", 6050)
+	.contentType("application/json")
+	.when()
+	.get("http://services.groupkt.com/state/get/{countryCode}/all", "IND")
+	.then()
+	.log().all()
+	;
+	//SerenityRest.setDefaultSessionId(bb.extract().sessionId());
+}
+@Step
+public void postreq(){
+	//then().statusCode(200);
+}
+@Step
+public void resp(){
+//rest().then().statusCode(200);
+	int id = Math.abs(new Random().nextInt());
+    Map<String, Object> jsonAsMap = new HashMap<>();
+    jsonAsMap.put("id", id);
+    jsonAsMap.put("name", "Dog");
+    jsonAsMap.put("status", "Active");
+    jsonAsMap.put("photoUrls", new ArrayList<>(Arrays.asList()));
+
+    given().contentType("application/json")
+    		.proxy("proxy.cognizant.com", 6050)
+            .content(jsonAsMap).log().body()
+            .baseUri("http://petstore.swagger.io")
+            .basePath("v2/pet")
+    .when()
+    .post();
+
 }
 
 
--- src/test/java/stepdefinitions/Apidefinitions.java
+++ src/test/java/stepdefinitions/Apidefinitions.java
@@ -5,6 +5,7 @@ import com.jayway.restassured.response.ResponseBody;
 
 import api.LoadRequest;
 import api.PostRequest;
+import api.Reqapi;
 import api.ValidateRequest;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
@@ -19,10 +20,15 @@ public static Response responsebody;
 
 	@Steps
 	LoadRequest loadrequest;
+	
 	@Steps
 	PostRequest postrequest;
+	
 	@Steps
 	ValidateRequest validateRequest;
+	
+	@Steps
+	Reqapi reqapi;
 	
 	@Given("^a request for Country \"([^\"]*)\"$")
 	public void a_GET_Method_request_for_Country(String arg1) throws Throwable {
@@ -31,7 +37,12 @@ public static Response responsebody;
 
 	@When("^request is sent to \"([^\"]*)\"$")
 	public void request_is_sent_to(String arg1) throws Throwable {
-		postrequest.postGETRequest(arg1);
+	//	postrequest.postGETRequest(arg1);
+	//	postrequest.giv();
+	//	postrequest.postreq();
+	//	postrequest.resp();
+		reqapi.r();
+	//	reqapi.p();
 	   
 	}
 
